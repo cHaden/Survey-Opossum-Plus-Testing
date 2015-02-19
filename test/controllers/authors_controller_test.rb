6d525cf9ea4fe378bdfb542fb7132bea29fe174d
C Haden <chaden@spoonflower.com>
2015-02-18 22:26:45 -0500

    five required tests pass

--- test/controllers/authors_controller_test.rb
+++ test/controllers/authors_controller_test.rb
@@ -2,4 +2,9 @@ require 'test_helper'
 
 class AuthorsControllerTest < ActionController::TestCase
 
+  #5. that a get request returns a success response (e.g. that a page loads successfully)
+  test "authors get new correctly" do
+    get :new
+    assert_response :success
+  end
 end
